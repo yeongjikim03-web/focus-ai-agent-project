198c580c3249b2c6b8fecf6b2018e495e7587d51
dddddhhhhh <imamy03@gmail.com>
2026-03-28 13:58:36 +0900

    Enhance logger with process and window tracking
    앱전환 or 앱전환안되고 10초 지났을때

--- logger.py
+++ logger.py
@@ -3,16 +3,17 @@ import json
 from datetime import datetime
 from pynput import keyboard, mouse
 import pygetwindow as gw
+import psutil
+import win32gui
+import win32process
 
 # ===== 전역 변수 =====
 keyboard_count = 0
 mouse_count = 0
-switch_count = 0
 
 last_input_time = time.time()
-current_window = None
-
-DURATION = 10  # 10초
+current_title = None
+current_app = None
 
 # ===== 키보드 이벤트 =====
 def on_key_press(key):
@@ -27,57 +28,84 @@ def on_click(x, y, button, pressed):
         mouse_count += 1
         last_input_time = time.time()
 
+# ==== 앱 이름 가져오기 ====
+def get_process_name():
+    try:
+        hwnd = win32gui.GetForegroundWindow()
+        if hwnd is not None:
+            thread_id, pid = win32process.GetWindowThreadProcessId(hwnd)
+            process = psutil.Process(pid)
+            app_name = process.name()
+            return app_name
+       
+        else:
+            return "Unknown"
+       
+    except:
+        return "Unknown"
+
+# ==== 창 이름 가져오기 ====
+
+def get_title_name():
+    try:
+        window = gw.getActiveWindow()
+        title = window.title if window else "Unknown"
+        return title
+    except:
+        return "Unknown"
+
+# ==== 현재 정보  ====
+prev_title = get_title_name()
+prev_app = get_process_name()
+present_time = time.time()
+
+
 # ===== 리스너 시작 =====
 keyboard.Listener(on_press=on_key_press).start()
 mouse.Listener(on_click=on_click).start()
 
+
 # ===== 메인 루프 =====
 while True:
+    current_title = get_title_name()
+    current_app = get_process_name()
 
-    start_time = time.time()
-    switch_count = 0  # 매 구간 초기화
+    # title이나 앱 달라질때 or 앱 안달라지고 10초 지났을때 
 
-    prev_window = None
+    app_change = current_title != prev_title or current_app != prev_app 
+   
+    if app_change or not app_change and (time.time() - present_time >= 10):
+        DURATION = round(time.time() - present_time, 2)
+       
+        # idle 계산
+        idle_time = int(time.time() - last_input_time)
+        is_idle = idle_time > 5
 
-    # 10초 동안 계속 체크
-    while time.time() - start_time < DURATION:
-        try:
-            window = gw.getActiveWindow()
-            title = window.title if window else "Unknown"
-        except:
-            title = "Unknown"
-
-        if prev_window is None:
-            prev_window = title
-        elif title != prev_window:
-            switch_count += 1
-            prev_window = title
-
-        time.sleep(0.5)  # 0.5초마다 체크
-
-    # 최종 창 정보
-    current_window = prev_window
-
-    # idle 계산
-    idle_time = int(time.time() - last_input_time)
-    is_idle = idle_time > 5
-
-    # 로그 생성
-    log = {
+        
+   
+        # 로그 생성
+        log = {
         "user_id": 1,
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "duration": DURATION,
-        "app": current_window,
-        "title": current_window,
+        "app": prev_app,
+        "title": prev_title,
         "keyboard_count": keyboard_count,
         "mouse_count": mouse_count,
-        "switch_count": switch_count,
         "idle_time": idle_time,
         "is_idle": is_idle
     }
 
-    print(json.dumps(log, indent=2, ensure_ascii=False))
+        print(json.dumps(log, indent=2, ensure_ascii=False))
+
+   
+    # 갱신
+        prev_title = current_title
+        prev_app = current_app
+        present_time = time.time()
 
     # 카운트 초기화
-    keyboard_count = 0
-    mouse_count = 0
+        keyboard_count = 0
+        mouse_count = 0
+
+    time.sleep(0.5)
