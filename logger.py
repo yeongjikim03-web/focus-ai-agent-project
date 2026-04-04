39b22779d6be49d16ba9ef76f5f962636f7ca581
riririmmm <mr8774@smail.kongju.ac.kr>
2026-04-04 17:12:05 +0900

    session_id 추가, sqlite로 raw데이터 저장

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 import time
 import json
+import sqlite3
 from datetime import datetime
 from pynput import keyboard, mouse
 import pygetwindow as gw
@@ -18,6 +19,73 @@ prev_app = None
 segment_start_time = time.time()
 
 lock = Lock()
+
+session_id = datetime.now().strftime("session_%Y%m%d_%H%M%S")
+
+
+# ===== DB 초기화  =====
+def init_db():
+    conn = sqlite3.connect("activity_logs.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS activity_raw_log (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        session_id TEXT NOT NULL,
+        user_id TEXT NOT NULL,
+        start_time TEXT NOT NULL,
+        end_time TEXT NOT NULL,
+        duration REAL NOT NULL,
+        app TEXT,
+        title TEXT,
+        keyboard_count INTEGER DEFAULT 0,
+        mouse_count INTEGER DEFAULT 0,
+        idle_time REAL DEFAULT 0,
+        is_idle INTEGER DEFAULT 0,
+        is_periodic INTEGER DEFAULT 0,
+        is_switch INTEGER DEFAULT 0,
+        long_idle INTEGER DEFAULT 0,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def save_raw_log(log):
+    conn = sqlite3.connect("activity_logs.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO activity_raw_log (
+        session_id, user_id,
+        start_time, end_time, duration,
+        app, title,
+        keyboard_count, mouse_count,
+        idle_time, is_idle,
+        is_periodic, is_switch, long_idle
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        log["session_id"],
+        log["user_id"],
+        log["start_time"],
+        log["end_time"],
+        log["duration"],
+        log["app"],
+        log["title"],
+        log["keyboard_count"],
+        log["mouse_count"],
+        log["idle_time"],
+        int(log["is_idle"]),
+        int(log["is_periodic"]),
+        int(log["is_switch"]),
+        int(log["long_idle"])
+    ))
+
+    conn.commit()
+    conn.close()
+
 
 # ===== 키보드 이벤트 =====
 def on_key_press(key):
@@ -57,10 +125,14 @@ def get_title_name():
     return None
 
 # ===== 초기 상태 =====
+init_db()
+
 initial_title = get_title_name()
 prev_title = initial_title if initial_title else "Unknown"
 prev_app = get_process_name()
 segment_start_time = time.time()
+# 로그 출력용
+start_time = datetime.now()
 
 # ===== 리스너 시작 =====
 keyboard.Listener(on_press=on_key_press).start()
@@ -82,9 +154,9 @@ while True:
     is_app_changed = (current_app != prev_app)
     is_title_changed = (current_title != prev_title)
 
-    
+
     is_switch = is_app_changed
- 
+
 
     is_periodic = (now - segment_start_time) >= 10
 
@@ -92,6 +164,7 @@ while True:
     if is_switch or is_periodic:
 
         duration = round(now - segment_start_time, 2)
+        end_time = datetime.now()
 
         with lock:
             k_count = keyboard_count
@@ -102,8 +175,11 @@ while True:
         is_idle = idle_time >= 5
 
         log = {
+            "session_id": session_id,
             "user_id": 1,
-            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+
+            "start_time": start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "end_time" : end_time.strftime("%Y-%m-%d %H:%M:%S"),
 
             "duration": duration,
             "is_periodic": is_periodic,
@@ -123,6 +199,7 @@ while True:
             "long_idle": idle_time >= 30
         }
 
+        save_raw_log(log)
         print(json.dumps(log, indent=2, ensure_ascii=False))
 
         # ===== 상태 업데이트 =====
@@ -131,6 +208,7 @@ while True:
 
         # 구간 리셋
         segment_start_time = now
+        start_time = end_time
 
         # 카운트 초기화 (구간 기준)
         with lock:
